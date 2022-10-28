f723b3e47293ac0d14598490516a93f85cb7a2ac
Crispin Stichart <95038310+CrispinStichart@users.noreply.github.com>
2022-10-28 11:21:23 -0400

    simplify

--- rot13.py
+++ rot13.py
@@ -1,20 +1,20 @@
-import string
 import win32clipboard as clip
 
 
-def rot13(s):
-    s = list(s)
-    for i, letter in enumerate(s):
-        letter: str
-        if not letter.isalpha():
+def rot13(s: str):
+    rotted = []
+    offset_lowercase = ord("a")
+    offset_uppercase = ord("A")
+    for c in s:
+        if not c.isalpha():
+            rotted.append(c)
             continue
-        alphabet = (
-            string.ascii_lowercase if letter.islower() else string.ascii_uppercase
-        )
-        j = alphabet.find(letter)
-        j = (j + 13) % 26
-        s[i] = alphabet[j]
-    return "".join(s)
+
+        offset = offset_lowercase if c.islower() else offset_uppercase
+        i = (ord(c) - offset + 13) % 26
+        rotted.append(chr(i + offset))
+
+    return "".join(rotted)
 
 
 clip.OpenClipboard()
